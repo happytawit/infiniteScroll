d10c1d828e0dd6e8e4eb7d24d76526a0202b7176
alaskov <laskov1996@gmail.com>
2024-03-27 11:19:30 +0300

    Добавлена функция fetchPosts, убраны лишние флаги, скорректирована работа searchParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import PostBlock from './components/PostBlock/PostBlock'
 import { Post } from './types/commonTypes'
@@ -22,26 +22,21 @@ function throttle(callee: any, timeout: any) {
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([])
-  const [limit, setLimit] = useState<number>(5)
-  const [isFetching, setIsFetching] = useState<boolean>(true)
-
   const [searchParams, setSearchParamas] = useSearchParams({
     limit: '5',
   })
 
-  // console.log(limit2)
-
-  // добавить fetchPosts, избавиться от isFetching
-  useEffect(() => {
-    if (isFetching) {
-      fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`)
-        .then((response) => response.json())
-        .then((result) => {
-          setPosts(result)
-        })
-        .finally(() => setIsFetching(false))
-    }
-  }, [isFetching])
+  function fetchPosts() {
+    fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${searchParams.get(
+        'limit',
+      )}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data)
+      })
+  }
 
   async function checkPosition() {
     // Нам потребуется знать высоту документа и высоту экрана:
@@ -62,42 +57,55 @@ function App() {
     // Отслеживаем, где находится низ экрана относительно страницы:
     const position = scrolled + screenHeight
 
-    if (position >= threshold) {
+    if (
+      position >= threshold &&
+      parseInt(searchParams.get('limit') as string, 10) < 25
+    ) {
       // Если мы пересекли полосу-порог, вызываем нужное действие.
-      setLimit((currLimit) => (currLimit <= 10 ? currLimit + 5 : currLimit))
-      setIsFetching(true)
-
-      console.log('searchParamas', searchParams)
-
-      const currentValue: string = searchParams.get('limit') || ''
-      const newValue = String(parseInt(currentValue, 10) + 5)
-
-      setSearchParamas({ limit: newValue })
-
-      console.log('searchParamas2', searchParams)
-
-      console.log('currentValue, newValue', currentValue, newValue)
-
-      // console.log(searchParams.get('limit'))
-      // console.log(searchParams)
-      // const limit2: string = searchParams.get('limit') || ''
-      // console.log(limit2)
-      // setSearchParamas({ limit: String(limit) })
+      // console.log(refLimitValue.current)
+      getNewPosts()
     }
   }
 
-  useEffect(() => {
-    console.log('limits in useeffect', searchParams.get('limit'))
-  }, [posts])
+  function setNewLimit() {
+    setSearchParamas((prev) => {
+      const cur: string = prev.get('limit') || ''
+
+      prev.set('limit', String(parseInt(cur, 10) + 5))
+
+      return prev
+    })
+  }
+
+  function getNewPosts() {
+    setNewLimit()
+    fetchPosts()
+  }
 
   useEffect(() => {
-    const handler = throttle(checkPosition, 250)
+    fetchPosts()
+  }, [])
 
-    window.addEventListener('scroll', () => checkPosition())
+  useEffect(() => {
+    const scrollHandler = throttle(checkPosition, 250)
+
+    window.addEventListener('scroll', scrollHandler)
 
     return () => {
-      document.removeEventListener('scroll', checkPosition)
+      window.removeEventListener('scroll', scrollHandler)
     }
+  }, [])
+
+  // куда правильно вставить скролл вниз?
+  useEffect(() => {
+    // console.log(posts.length)
+    setTimeout(() => {
+      if (parseInt(searchParams.get('limit') as string, 10) > 5) {
+        // console.log('scrollTo')
+        // console.log(document.body.scrollHeight)
+        window.scrollTo(0, document.body.scrollHeight)
+      }
+    }, 1000)
   }, [])
 
   return (
@@ -108,13 +116,13 @@ function App() {
             return <PostBlock key={post.id} post={post} />
           })}
         </div>
-        {limit >= 10 || limit < 100 ? (
+        {parseInt(searchParams.get('limit') as string, 10) >= 10 ||
+        parseInt(searchParams.get('limit') as string, 10) < 100 ? (
           <Button
             className="buttonAdd"
             innerText="Загрузить еще"
             onClick={() => {
-              setLimit((currLimit) => currLimit + 5)
-              setIsFetching(true)
+              getNewPosts()
             }}
           />
         ) : null}
